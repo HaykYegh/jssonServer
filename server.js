4e814b058a554c01832e3ea3857e74e9ddc1b487
HaykYegh <haykyeghpersonal@gmail.com>
2023-07-11 06:05:28 -0700

    added cridentials

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post('/login', async (req, res) => {
       res.cookie('token', token, {
         httpOnly: true, // Make the cookie HTTP-only
         secure: false, // Set secure flag for HTTPS
-        sameSite: 'none', // Prevent CSRF
+        sameSite: 'lax', // Or 'none'
       });
 
       res.status(200).json({ message: 'User authenticated successfully', token });
