b6f34252e89749e6c0ca3d01a66b66955a9162ee
HaykYegh <haykyeghpersonal@gmail.com>
2023-07-11 09:31:36 -0700

    added cridentials

--- server.js
+++ server.js
@@ -79,10 +79,7 @@ app.post('/login', async (req, res) => {
       // User authenticated successfully, generate a JWT
       const { password, ...userInfoWithoutPassword } = user;
       let token = jwt.sign(userInfoWithoutPassword, process.env.JWT_SECRET, { expiresIn: '1h' });
-      res.cookie('token', token, {
-        secure: true, // Set secure flag for HTTPS
-        sameSite: 'none', // Prevent CSRF
-      });
+      res.cookie('token', token);
 
       res.status(200).json({ message: 'User authenticated successfully', token });
     } else {
